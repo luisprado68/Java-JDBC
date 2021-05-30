f7d9c51ec435d6f281e6b7ecd2aca1cf7263d177
Luis Prado <luisprado0095@gmail.com>
2021-05-30 15:58:23 -0300

    :memo

---
+++ 176-ManejoJDBC/src/main/java/datos/Conexion.java
@@ -0,0 +1,7 @@
+
+package datos;
+
+
+public class Conexion {
+    private static final String JBDC_URL = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+}
